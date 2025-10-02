627c9ae3bfc39347325d897fa6a4c6e40283a3bc
amirmatics <4miralbekord@gmail.com>
2025-10-01 18:05:38 -0700

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import BaristaForm from './components/BaristaForm'
 function App() {
 
   return (
     <>
-    <h2>Hi, I'd like to order a:</h2>
-    <div className="title-container">
-      <h1 className="title">On My Grind</h1>
-      <p>So you think you can barista? Let's put that to the test...</p>
-    </div>
-    <BaristaForm />
     </>
   )
 }
