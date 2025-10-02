ca6699f205a6e5b9839af0afed5943c2719acfd2
amirmatics <4miralbekord@gmail.com>
2025-10-01 23:47:59 -0700

    initial setup

---
+++ src/components/BaristaForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const BaristaForm = () => {
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+    });
+    const ingredients = {
+        'temperature': ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+    }
+    const onNewDrink = () => {
+
+    };
+    const onCheckAnswer = () => {
+
+    };
+
+    return (
+        <div>
+            <h2>Hi, I'd like to order a:</h2>
+            <form >
+
+            </form>
+
+            <button className="button submit" onClick={onCheckAnswer}>
+                Check Answer
+            </button>
+
+            <button className="button newdrink" onClick={onNewDrink}>
+                New Drink
+            </button>
+
+        </div>
+    );
+
+};
+
+export default BaristaForm;
