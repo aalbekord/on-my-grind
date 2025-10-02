a8de982a64b24adff6859bf4585f5d072fdd2589
amirmatics <4miralbekord@gmail.com>
2025-10-02 10:35:22 -0700

    lab complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
     <div className="title-container">
+      <img src="https://static.vecteezy.com/system/resources/previews/068/635/709/non_2x/coffee-cup-with-latte-art-on-white-background-free-vector.jpg" alt="cofee bag logo" className="logo"/>
       <h1 className="title">On My Grind</h1>
       <p>So you think you can barista? Let's put that to the test...</p>
     </div>
