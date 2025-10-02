e60798ddf17191a804b16e9d12e5b93699dad2c2
amirmatics <4miralbekord@gmail.com>
2025-10-02 10:19:46 -0700

    technical details down

--- src/components/RecipeChoices.jsx
+++ src/components/RecipeChoices.jsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import "./RecipeChoices.css"
 
 const RecipeChoices = ({ handleChange, label, choices, checked }) => {
-    return (
-      <div className="radio-buttons">
-        {choices &&
-          choices.map((choice) => (
-            <li key={choice}>
-              <input
-                id={choice}
-                value={choice}
-                name={label}
-                type="radio"
-                onChange={handleChange}
-                checked={checked == choice}
-              />
-                {choice}
-            </li>
-          ))}
-      </div>
-    );
+  
+  return (
+    <div className="radio-buttons">
+      <input
+        type="text"
+        name={label}
+        value={checked}
+        placeholder="Guess the ingredient..."
+        onChange={handleChange}
+        className = "textbox"
+      />
+
+      {choices &&
+        choices.map((choice) => (
+          <li key={choice}>
+        
+            {choice}
+            
+          </li>
+      ))}
+    </div>
+  );
 };
 
 export default RecipeChoices;
