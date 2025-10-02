306f2522009f4de9b76cbc51d650154471578cb5
amirmatics <4miralbekord@gmail.com>
2025-10-01 17:22:45 -0700

    created baristaform file

---
+++ src/components/BaristaForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+const BaristaForm = () => {
+  
+  return (
+  <div>
+    <form >
+      
+      // we will fill this in soon!
+      
+    </form>
+
+    <button className="button submit" onClick={onCheckAnswer}>
+      Check Answer
+    </button>
+
+    <button className="button newdrink" onClick={onNewDrink}>
+      New Drink
+    </button>
+  </div>
+  );
+  
+};
+
+export default BaristaForm;
